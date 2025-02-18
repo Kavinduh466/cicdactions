e6fbdf94913fddac405022df49fe3d0fe6c90bd5
Kavindu Herath <kavinduh466@gmail.com>
2025-02-18 14:31:50 +0530

    commit

---
+++ src/main/java/com/example/cicdactions/CicdactionsApplication.java
@@ -0,0 +1,14 @@
+package com.example.cicdactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdactionsApplication.class, args);
+        System.out.println("Cicdactions Application Started");
+    }
+
+}
